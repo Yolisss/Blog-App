5317b17a88cd27fd491655c067c44d3b620fe160
Yolis <yolisisabel28@gmail.com>
2022-10-12 11:08:09 -0700

    created database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors";
+import db from "./db/db-connection";
+
+//body parser processes data sent in an http req body aka req.body
+//"this is how its processing the data"
+import bodyParser from "body-Parser";
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.json("hello from techtonica");
